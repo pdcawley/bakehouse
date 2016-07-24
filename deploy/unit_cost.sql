7c16ffbfedeb582600e55123df685daee4ddf7f5
Piers Cawley <pdcawley@bofh.org.uk>
2016-07-24 02:43:39 +0100

    Test and fix unit_cost
    This might prove to be a bad assumption, the unit_cost table works out
    the cost of ingredients and intermediate stages at a per kilogram rate,
    but the product cost is the cost for a finished product. Where a
    finished product is (say) 6 scotch rolls, then the 'unit' is taken to be
    a 'batch of six rolls'.
    
    Note too that this 'per_unit' cost is solely the cost of ingredients.
    The still to come 'cost_breakdown' view is where we calculate that.

--- deploy/unit_cost.sql
+++ deploy/unit_cost.sql
@@ -5,25 +5,52 @@
 BEGIN;
 
 CREATE VIEW bakehouse.unit_cost AS WITH RECURSIVE i AS (
-  SELECT ingredient AS name, pack_size AS amount, ingredient AS ingredient, pack_size AS total_amount, price / pack_size AS cost
-  FROM bakehouse.ingredient
-), recipe_size AS (
-  SELECT recipe, sum(amount) as total_amount FROM bakehouse.recipe_item GROUP BY recipe
-), ri AS (
-  SELECT recipe_item.recipe AS name, amount, ingredient, recipe_size.total_amount total_amount
-  FROM bakehouse.recipe_item AS recipe_item
-  JOIN recipe_size ON recipe_size.recipe = recipe_item.recipe
-), reduced AS (
-SELECT name, amount AS amount, ingredient, amount AS total_amount FROM i
+  SELECT ingredient AS name,
+         pack_size AS amount,
+         ingredient AS ingredient,
+         pack_size AS total_amount,
+         price / pack_size AS cost
+    FROM bakehouse.ingredient
+),
+recipe_size AS (
+  SELECT recipe,
+         sum(amount) as total_amount
+    FROM bakehouse.recipe_item
+GROUP BY recipe
+),
+ri AS (
+  SELECT recipe_item.recipe AS name,
+         amount,
+         ingredient,
+         recipe_size.total_amount AS total_amount
+    FROM bakehouse.recipe_item AS recipe_item
+    JOIN recipe_size ON recipe_size.recipe = recipe_item.recipe
+),
+reduced AS (
+  SELECT name,
+         amount AS amount,
+         ingredient,
+         amount AS total_amount
+    FROM i
 UNION ALL
-  SELECT ri.name, (ri.amount * (i.amount / i.total_amount)), i.ingredient, ri.total_amount
-  FROM ri
-  JOIN reduced i ON ri.ingredient = i.name
+  SELECT ri.name,
+         (ri.amount * (i.amount / i.total_amount)),
+         i.ingredient,
+            ri.total_amount
+    FROM ri
+    JOIN reduced i ON ri.ingredient = i.name
 )
-SELECT name, SUM(cost / total_amount) AS cost
-FROM (SELECT r.name, sum(r.amount) * max(i.cost) AS COST, r.total_amount
-      FROM reduced r
-      JOIN i ON i.name = r.ingredient
+SELECT name,
+       SUM(cost / total_amount)
+     * COALESCE((SELECT scale_weight * pieces
+                 FROM bakehouse.recipe
+                 WHERE recipe.recipe = blah.name)
+               , 1) AS cost
+  FROM (SELECT r.name,
+               sum(r.amount) * max(i.cost) AS cost,
+               r.total_amount
+          FROM reduced r
+          JOIN i ON i.name = r.ingredient
       GROUP BY r.name, r.ingredient, r.total_amount) AS blah
 GROUP BY name, total_amount;
 
